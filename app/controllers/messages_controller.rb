532c16cf219389b61957a374cca4ddc90536f9c7
wtoshiaki <88226506+wtoshiaki@users.noreply.github.com>
2021-10-19 09:01:01 +0900

    非同期ファイル作成修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,12 +8,16 @@ class MessagesController < ApplicationController
    def create
      @room = Room.find(params[:room_id])
      @message = @room.messages.new(message_params)
+    #  if @message.save
+    #    redirect_to room_messages_path(@room)
+    #  else
+    #   @messages = @room.messages.includes(:user)
+    #   render :index
+    #  end
+
      if @message.save
-       redirect_to room_messages_path(@room)
-     else
-      @messages = @room.messages.includes(:user)
-      render :index
-     end
+      ActionCable.server.broadcast 'message_channel', content: @message
+    end
    end
  
  
